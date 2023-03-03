4022d6e0ab0a9c5762b52b39777920ed0b98354b
fernando-daw <flagonovoa@danielcastelao.org>
2023-03-03 12:46:16 +0100

    instanciamos en el Main la calculadora

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ public class Main {
         EntradaSalida.letras("Introduce el mensaje que quieras");
         EntradaSalida.numero("Introduce el numero entero que quieras");
 
+        EntradaSalida.calculadora(5,6,suma);
+
 
     }
 }
